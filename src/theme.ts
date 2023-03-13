de0310d3e909c62e15111a16bef7d3d66eb60eb4
dltmdwo05 <dltmdwo05@naver.com>
2023-03-13 16:36:01 +0900

    #6.5 ToDoList setup

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: 'whitesmoke',
+  accentColor: '#8c7ae6',
+  exBlack: 'black',
+  exWhite: 'whitesmoke',
+};
