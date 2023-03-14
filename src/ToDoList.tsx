450cdec52309d622e3bd7ed96db9e4d3a06a5350
dltmdwo05 <dltmdwo05@naver.com>
2023-03-14 16:50:45 +0900

    officeversion 복사(#6.9), #6.10 Recap

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface IForm {
+  toDo: string;
+}
 
 function ToDoList() {
-  const [toDo, setToDo] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log('add to do', data.toDo);
+    setValue('toDo', '');
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register('toDo', {
+            required: 'Please write a To Do...',
+          })}
+          placeholder="Write a to do"
+        />
         <button>Add</button>
       </form>
     </div>
